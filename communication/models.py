ac58b378f8d4942498b745eb2c182d95de723db2
reza <rezamazarloo@yahoo.com>
2026-07-04 08:58:35 +0330

    added incoming messages model

--- communication/models.py
+++ communication/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class IncomingMessage(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="incoming_messages",
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_id} {self.created_at}"
