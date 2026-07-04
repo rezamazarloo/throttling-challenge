c760a18ed681d8280a1cbcda4850e13a04bc7ed1
reza <rezamazarloo@yahoo.com>
2026-07-04 08:13:13 +0330

    rename register to signup view in url

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from account.views import LogoutView, RegisterView
+from account.views import LogoutView, SignupView
 
 app_name = "account"
 
 urlpatterns = [
-    path("auth/signup/", RegisterView.as_view(), name="signup"),
+    path("auth/signup/", SignupView.as_view(), name="signup"),
     path("auth/login/", obtain_auth_token, name="login"),
     path("auth/logout/", LogoutView.as_view(), name="logout"),
 ]
