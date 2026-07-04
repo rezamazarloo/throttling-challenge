780c7ce456ccc77c4d5b4fb4adbb465708f9fe96
reza <rezamazarloo@yahoo.com>
2026-07-04 10:30:29 +0330

    modified models and migrations

--- core/models.py
+++ core/models.py
@@ -5,12 +5,15 @@ from django.db import models
 class RequestLog(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        db_index=False,
         related_name="request_logs",
     )
     endpoint = models.CharField(max_length=255)
-    accepted = models.BooleanField()
+    status = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"user #{self.user_id} | {self.endpoint} | {self.accepted}"
+        return f"user #{self.user_id} | {self.endpoint} | {self.status}"
