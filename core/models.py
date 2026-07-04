18fd0adbdf5f21d3079d7e972c7757d6825c7945
reza <rezamazarloo@yahoo.com>
2026-07-04 08:56:52 +0330

    added request log model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class RequestLog(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="request_logs",
+    )
+    endpoint = models.CharField(max_length=255)
+    accepted = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"user #{self.user_id} | {self.endpoint} | {self.accepted}"
