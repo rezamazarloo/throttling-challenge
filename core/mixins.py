4144ca4954e66ac0539dfbcc73eab2570108ce98
reza <rezamazarloo@yahoo.com>
2026-07-04 12:22:01 +0330

    added mixins for log request on specific views & count user limit only on success api calls

---
+++ core/mixins.py
@@ -0,0 +1,26 @@
+from core.models import RequestLog
+
+
+class RequestLogMixin:
+    def finalize_response(self, request, response, *args, **kwargs):
+        response = super().finalize_response(request, response, *args, **kwargs)
+
+        user = request.user if request.user.is_authenticated else None
+        RequestLog.objects.create(
+            user=user,
+            endpoint=request.path,
+            status=response.status_code,
+        )
+
+        return response
+
+
+class ThrottleFinalizeMixin:
+    def finalize_response(self, request, response, *args, **kwargs):
+        response = super().finalize_response(request, response, *args, **kwargs)
+
+        for throttle_class in self.throttle_classes:
+            if hasattr(throttle_class, "finalize_response"):
+                throttle_class.finalize_response(request, response, self)
+
+        return response
